4c411698c8ec73a32c9ed967d3b7ea7a1f639a9c
Joe Chen <joejinghongchen@familysearch.org>
2018-09-18 23:47:29 -0600

    Initial Commit

---
+++ 4380_vm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  4380_vm
+//
+//  Created by jing hong chen on 9/18/18.
+//  Copyright © 2018 jing hong chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
